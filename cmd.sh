418d602b9236f2e0e8e03ac4e6a316a662cb95c5
qiaofeng1227 <76487013@qq.com>
2022-05-28 09:12:52 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,2 @@
-app_pass=$(echo -n $APP_PASSWORD | sha1sum|awk 'BEGIN{ORS=""}{print $1}'|sha1sum|awk '{print $1}')
-mysqladmin -uroot -p$APP_PASSWORD -h 127.0.0.1 redmine -e "update users set hashed_password = '$app_pass', salt ='', admin = 1 where login = 'admin';"
+app_pass=$(echo -n $REDMINE_PASSWORD | sha1sum|awk 'BEGIN{ORS=""}{print $1}'|sha1sum|awk '{print $1}')
+mysqladmin -uroot -p$REDMINE_PASSWORD -h 127.0.0.1 redmine -e "update users set hashed_password = '$app_pass', salt ='', admin = 1 where login = 'admin';"
