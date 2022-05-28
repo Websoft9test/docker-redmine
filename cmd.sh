8043ece76fe4d999f304912f1cca4088b981a4d0
qiaofeng1227 <76487013@qq.com>
2022-05-28 09:43:34 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,5 @@
+apt update
+apt install -y mariadb-client
 app_pass=$(echo -n $REDMINE_PASSWORD | sha1sum|awk 'BEGIN{ORS=""}{print $1}'|sha1sum|awk '{print $1}')
 mysqladmin -uroot -p$REDMINE_PASSWORD -h 127.0.0.1 redmine -e "update users set hashed_password = '$app_pass', salt ='', admin = 1 where login = 'admin';"
 
