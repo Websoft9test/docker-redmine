6dbdb1ebf4b63ba4e1da0cce4669688f4c587982
qiaofeng1227 <76487013@qq.com>
2022-06-02 08:28:14 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,6 +1,8 @@
+rails server -b 0.0.0.0
 apt update
 apt install -y mariadb-client
 app_pass=$(echo -n $REDMINE_PASSWORD | sha1sum|awk 'BEGIN{ORS=""}{print $1}'|sha1sum|awk '{print $1}')
 mysql -uroot -p$REDMINE_PASSWORD -h db redmine -e "update users set hashed_password = '$app_pass', salt ='', admin = 1 where login = 'admin';"
 
-/docker-entrypoint.sh rails server -b 0.0.0.0
+tail -f /dev/null
+
