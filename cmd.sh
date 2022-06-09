f97d11d58b1eb6b30d98b5aa7657432a7b7b3dae
qiaofeng1227 <76487013@qq.com>
2022-06-09 13:39:02 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo $REDMINE_PASSWORD
-app_pass=$(echo -n admin | sha1sum |awk '{print $1}')
+app_pass=$(echo -n $REDMINE_PASSWORD | sha1sum |awk '{print $1}')
 echo $app_pass
 sed -i "s/d033e22ae348aeb5660fc2140aec35850c4da997/$app_pass/g" /usr/src/redmine/db/migrate/001_setup.rb
 cat /usr/src/redmine/db/migrate/001_setup.rb
